09b7b6c9372f4a8db90f705d18709e932de47808
unknown <mauricio.ar.requena@gmail.com>
2025-03-20 11:49:19 -0400

    added styels for sidenav

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SharedAddTaskService } from '../../services/shared-add-task/shared-add-task.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule],
+  imports: [MatSidenavModule, MatButtonModule, MatIconModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
